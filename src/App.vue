9817d93f560034a692fd50cb51a95e761056cf70
Mulia Arifandi Nasution <mul14.net@gmail.com>
2018-05-29 00:11:07 +0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit.prevent="login">
+      <input type="text" v-model="form.username">
+      <input type="password" v-model="form.password">
+      <button v-if="!loggedin" type="submit">Login</button>
+      <button v-if="!loggedin" type="button" @click="register">Register</button>
+      <button v-if="loggedin" type="button" @click="logout">Logout</button>
+    </form>
+    <button @click="fetchArticles()">Get All Articles</button>
+    <button @click="fetchArticles(1)">Get article 1</button>
+    <button @click="fetchArticles(2)">Get article 2</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from "axios"
+  import Cookies from "js-cookie"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        loggedin: false,
+        form: {
+          username: 'mul14',
+          password: '123',
+        }
+      }
+    },
+
+    mounted() {
+      this.loggedin = !!axios.defaults.headers.common['Authorization']
+    },
+
+    methods: {
+
+      logout() {
+        delete axios.defaults.headers.common['Authorization']
+        Cookies.remove('token')
+        this.loggedin = false
+      },
+
+      login() {
+        axios.post('auth/login', {
+          username: this.form.username,
+          password: this.form.password,
+        }).then(res => {
+          if (res.data.token) {
+            Cookies.set('token', res.data.token)
+            axios.defaults.headers.common['Authorization'] = Cookies.get('token')
+            this.loggedin = true
+          }
+        })
+      },
+
+      register() {
+        axios.post('auth/register', {
+          username: this.form.username,
+          password: this.form.password,
+        }).then(res => {
+          if (res.data.token) {
+            Cookies.set('token', res.data.token)
+            axios.defaults.headers.common['Authorization'] = Cookies.get('token')
+          }
+        })
+      },
+
+      fetchArticles(id) {
+        if (id) {
+          axios.get('articles/' + id)
+        } else {
+          axios.get('articles')
+        }
+      }
+
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import Cookies from 'js-cookie'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.headers.common['Authorization'] = Cookies.get('token')
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+
 
 new Vue({
   render: h => h(App)
